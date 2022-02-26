778553f7db715dd148f1d35c8616ac8ac840c127
Wisdom <adeniyi.wisdom@gmail.com>
2022-02-26 22:00:16 +0100

    Update messagebox.py

--- messagebox.py
+++ messagebox.py
@@ -2,6 +2,6 @@
 From tkinter import messagebox
 
 userinput=input("Please enter your quotation ") #user input in the program
-myText = "I have a dream to achieve"
+myText = "I have a dream"
 ourinput=myText + userinput
 messagebox.showinfo("Welcome",ourinput) #message box display on the screen
