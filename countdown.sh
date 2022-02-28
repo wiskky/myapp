293d1571acb5c63b22526e8c36d20ab8c5c6b6b5
wiskky <adeniyi.wisdom@gmail.com>
2022-02-28 22:51:24 +0100

    Update file

--- countdown.sh
+++ countdown.sh
@@ -3,10 +3,10 @@
 #this program print number in a primal style.
 
 a=0
-while [ "$a" -lt 10 ]   
+while [ "$a" -lt 10 ]
 do
    b="$a"
-   while [ "$b" -ge 0 ]  
+   while [ "$b" -ge 0 ]
    do
       echo -n "$b "
       b=`expr $b - 1`
@@ -19,6 +19,7 @@ done
 #!/bin/sh
 # Script to print currently logged in users information, and current date & time.
 #This is for personal study
+#To be continued.
 clear
 echo "Hello $USER"
 echo -e "Today is \c ";date
