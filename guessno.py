c48a79afb852f2f7d2d57a5cf711dd982d21aac9
Wisdom <adeniyi.wisdom@gmail.com>
2022-03-04 23:02:00 +0100

    Update guessno.py

--- guessno.py
+++ guessno.py
@@ -1,8 +1,8 @@
 #Program to allow user to enter the guess number from 1 to 10
 import random
 
-def guess(x):
-    random_number = random.randint(1, x)
+def guess(guessnumber):
+    random_number = random.randint(1, guessnumber)
     guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
@@ -12,4 +12,4 @@ def guess(x):
             print('Sorry, guess again. Too high.')
     print(f'Yay, congrats. You have guessed the {random_number}')
 
-guess(40)
+guess(50)
