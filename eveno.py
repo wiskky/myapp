f16fd4b3b10afbf6d6431096a4064c3afba41193
Wisdom <adeniyi.wisdom@gmail.com>
2022-03-07 21:55:38 +0100

    Update eveno.py

--- eveno.py
+++ eveno.py
@@ -5,3 +5,9 @@ for number in range(1,30):
         a_list.append(number)
 
 print(a_list)
+
+
+#This sprogram is the same as the first one.
+
+b_list = [ number for number in range(1, 30) if number % 2 == 0 ]
+print(b_list)
