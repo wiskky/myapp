8de691aec499da6d6facdb043c9230461612d7ea
Wisdom <adeniyi.wisdom@gmail.com>
2022-03-08 21:44:09 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+#This program printing the even number from 1 to 30
+
+a_list = []
+
+for number in range(1,40):
+    if number % 2 == 0:
+        a_list.append(number)
+
+print(a_list)
+
+#This sprogram is the same as the first one.
+
+b_list = [ number for number in range(1, 30) if number % 2 == 0 ]
+print(b_list)
