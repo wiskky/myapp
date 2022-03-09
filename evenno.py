758a45cbf0f05429f4b84227ebd485eb6499af5b
wiskky <adeniyi.wisdom@gmail.com>
2022-03-09 21:40:09 +0100

    unpdate file

--- evenno.py
+++ evenno.py
@@ -2,7 +2,7 @@
 
 a_list = []
 
-for number in range(1,20):
+for number in range(1,40):
     if number % 2 == 0:
         a_list.append(number)
 
@@ -10,7 +10,7 @@ print(a_list)
 
 #This sprogram is the same as the first one.
 
-b_list = [ number for number in range(1, 30) if number % 2 == 0 ]
+b_list = [ number for number in range(1, 40) if number % 2 == 0 ]
 print(b_list)
 
 
