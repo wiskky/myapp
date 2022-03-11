9dc9a955cb67d14b8efe282cfd2f071bf4b2f68c
wiskky <adeniyi.wisdom@gmail.com>
2022-03-11 18:54:10 +0100

    updated guessno.py

--- guessno.py
+++ guessno.py
@@ -1,6 +1,10 @@
 #Program to allow user to enter the guess number from 1 to 10
 import random
 
+#This program allows user to guess the correct number from 1 to 10
+#system will ask user to enter the guess number and if is correct the user wins
+# if not the user will continue to enter the number until it guess it right.
+
 def guess(guessnumber):
     random_number = random.randint(1, guessnumber)
     guess = 0
