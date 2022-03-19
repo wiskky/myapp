c69b7006b61da5c0f4d908d34f3e2780e7a0d3af
wiskky <adeniyi.wisdom@gmail.com>
2022-03-19 17:04:19 +0100

    commit stdmrks.py

---
+++ stdmarks.py
@@ -0,0 +1,10 @@
+name_list = ["Toyin", "Remi", "Bukky", "Femi"]
+marks = [95, 48, 59, 75]
+
+counter = 0
+
+for students in name_list:
+    if students == "Bukky":
+        print(marks[counter])
+        break
+    counter += 1
