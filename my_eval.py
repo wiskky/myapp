7ccf01a2c488e8c2d55ef2b47027a66aec093e0e
Wisdom <adeniyi.wisdom@gmail.com>
2022-03-24 09:14:54 +0100

    Update my_eval.py

--- my_eval.py
+++ my_eval.py
@@ -2,7 +2,7 @@
 #print(dir(os))
 
 
-#how to use eval function in python
+#how to use eval function in python.
 #This is just a skeleton program
 while True:
 	print(eval(input("Enter Expression: ")))
