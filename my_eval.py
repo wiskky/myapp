ca71e91e477d4d375a3d9ed99e98b9469e962f52
wiskky <adeniyi.wisdom@gmail.com>
2022-03-24 09:09:36 +0100

    commit my_eval.py

--- my_eval.py
+++ my_eval.py
@@ -1,9 +1,17 @@
 #how to use eval function in python
 #This is just a skeleton program
-while True:
-	print(eval(input("Enter Expression: ")))
+#while True:
+#	print(eval(input("Enter Expression: ")))
 
 	
 
-#import os
+import os
+
+from virtualenv import cli_run
 #print(dir(os))
+
+# list files and directories in the working directory
+
+current_dir =os.getcwd()
+for entry in os.listdir(current_dir):
+	print(entry)
