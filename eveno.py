7f21490f8e788c6ecb8fb186cadb2bbb95f5b10e
Wisdom <adeniyi.wisdom@gmail.com>
2022-04-09 20:34:22 +0100

    Update eveno.py

--- eveno.py
+++ eveno.py
@@ -9,5 +9,5 @@ print(a_list)
 
 #This sprogram is the same as the first one.
 
-b_list = [ number for number in range(1, 40) if number % 2 == 0 ]
+b_list = [ number for number in range(1, 50) if number % 2 == 0 ]
 print(b_list)
