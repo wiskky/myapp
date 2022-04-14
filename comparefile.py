17f3a37b1f92553372df9e5f9e56168f9f167adb
wiskky <adeniyi.wisdom@gmail.com>
2022-04-14 20:05:17 +0100

    updated file

--- comparefile.py
+++ comparefile.py
@@ -4,5 +4,5 @@ import sys
 
 my_list = [0, 1, 2, 3, 4, 5, "Joy", False]
 my_tuple = (0, 1, 2, 3, 4, 5, "Joy", False)
-print(sys.getsizeof(my_list), "bytes")
-print(sys.getsizeof(my_tuple), "bytes")
+print("The list file is ", sys.getsizeof(my_list), "bytes")
+print("The tuple file is ", sys.getsizeof(my_tuple), "bytes")
