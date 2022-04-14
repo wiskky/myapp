bcf09447d0f83b6c6c41d41f78097fe4874da866
Wisdom <adeniyi.wisdom@gmail.com>
2022-04-14 19:59:07 +0100

    Update comparefile.py

--- comparefile.py
+++ comparefile.py
@@ -1,6 +1,7 @@
 import sys
 
 #This program is comparing the size of the list and tuple
+#to know the one that has less bytes.
 
 my_list = [0, 1, 2, 3, 4, 5, "Joy", False]
 my_tuple = (0, 1, 2, 3, 4, 5, "Joy", False)
