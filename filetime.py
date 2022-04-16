01a845834521292d9b219221c3e39802f33832eb
Wisdom <adeniyi.wisdom@gmail.com>
2022-04-16 21:02:48 +0100

    Update filetime.py

--- filetime.py
+++ filetime.py
@@ -1,4 +1,6 @@
 import timeit
 
+#This program calculate the execution time of the two statement
+
 print(timeit.timeit(stmt="[0,1,2,3,4,5]",number=1000000))
 print(timeit.timeit(stmt="(0,1,2,3,4,5)",number=1000000))
