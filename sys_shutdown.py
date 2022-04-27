8aab3cf6ad12cffe3a40de6e9af4e6c3639b4dd2
wiskky <adeniyi.wisdom@gmail.com>
2022-04-27 17:44:00 +0100

    updating sys_shutdown.py

--- sys_shutdown.py
+++ sys_shutdown.py
@@ -1,5 +1,7 @@
 
 #This program shutdown the system/computer in one second
 
+#PLease dont run this program, it will shutdown your system
+
 import os
-os.system("shutdown /s /t 5")
+#os.system("shutdown /s /t 5")
