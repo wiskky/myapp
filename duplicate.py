e1645ba0b44e4b94028625f3a8654f05a796c9af
wiskky <adeniyi.wisdom@gmail.com>
2022-04-27 15:18:30 +0100

    Commit duplicate.py

---
+++ duplicate.py
@@ -0,0 +1,12 @@
+#This programe check for the duplicate number in the list and print it out.
+num = [20,30,20,40,30,50,70,90,50,100]
+
+new = []
+
+for i in num:
+	n = num.count(i)
+	if n > 1:
+		if new.count(i) == 0:
+			new.append(i)
+
+print(new)
