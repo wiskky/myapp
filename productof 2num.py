9570274eca0d7a112e846eb695d19d1319f0ab94
wiskky <adeniyi.wisdom@gmail.com>
2022-05-03 21:32:28 +0100

    update file

--- productof 2num.py
+++ productof 2num.py
@@ -4,10 +4,13 @@ Given an array of integers, find the pair of adjacent elements
 that has the largest product and return that product
 
 '''
-
+'''
 # THis program is multiplying the element in the list and print out the max no.
 def max_of_product_num(nums):
 	return ([a * b for a, b in zip(nums, nums[1:])])
 
 my_list =  max_of_product_num([1,2,3,4,5,6])
 print(max(my_list))
+
+'''
+
