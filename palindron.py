e161770703036354955f994f336af76019f65a3b
wiskky <adeniyi.wisdom@gmail.com>
2022-05-03 21:31:35 +0100

    updated file

--- palindron.py
+++ palindron.py
@@ -1,3 +1,4 @@
+'''
 def checkPalindron(str):
 	my = str
 	rev = str[::-1]
@@ -9,3 +10,13 @@ def checkPalindron(str):
 
 p = checkPalindron('saas')
 print(p)
+'''
+
+"""
+nums = [1,2,3,4,5,6]
+w =  [a * b for a, b in zip(nums, nums[1:])]
+print (min(list(w)))
+
+"""
+
+
