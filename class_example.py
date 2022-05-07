fee2717e80811005c7085ea3d126ce876fbda96b
wiskky <adeniyi.wisdom@gmail.com>
2022-05-07 20:06:09 +0100

    updated files

--- class_example.py
+++ class_example.py
@@ -14,7 +14,7 @@ class Vehicle:
     name = ""
     kind = "car"
     color = ""
-    value = 100.00
+    value = 500.00
     def description(self):
         #desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
         return(f"{self.name} is a {self.color} {self.kind} worth {self.value}")
