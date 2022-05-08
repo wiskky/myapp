7f8427862748d9e21994e4fd40d93845b55a1ba5
wiskky <adeniyi.wisdom@gmail.com>
2022-05-08 20:47:09 +0100

    updated file

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,12 @@
 # Definition of dictionary
-europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+europe = {'spain':'madrid', 'france':'paris', 'germany':'ronn', 'norway':'oslo' }
 
 # Print out the keys in europe
 print(europe.keys())
+print(europe)
+
+# Update capital of germany
+europe['germany'] = 'berlin'
 
 # Print out value that belongs to key 'norway'
 print(europe['norway'])
