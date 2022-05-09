24e804a1fb6ec2f6c85dadc8a085197683e81616
wiskky <adeniyi.wisdom@gmail.com>
2022-05-09 16:47:00 +0100

    Updating file

--- file_error.py
+++ file_error.py
@@ -1,6 +1,6 @@
 #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes 
 # in position 2-3: truncated \UXXXXXXXX escape
-#C:\Users\Documents\cars.csv',
+#C:\Users\Documents\cars.csv', this is the directory of the file
 
 import pandas as pd
 
@@ -9,5 +9,8 @@ cars = pd.read_csv(r'C:\Users\Documents\cars.csv', index_col=0)
 
 #You can also add another back slash to it as the 2nd method to avoid this error
 #cars = pd.read_csv('C:\\Users\\Documents\\cars.csv', index_col=0)
+
+#You can change back slash to forward slach  as the 3rd method to avoid this error
+#cars = pd.read_csv('C:/Users/Documents/cars.csv', index_col=0)
 
 print(cars)
