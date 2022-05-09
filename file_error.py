19bf1f0b90387d2b691a5f3207d8a0eb1191b683
wiskky <adeniyi.wisdom@gmail.com>
2022-05-09 16:44:07 +0100

    Updating file

--- file_error.py
+++ file_error.py
@@ -7,4 +7,7 @@ import pandas as pd
 #To avoid error as the output,m kindly put r at the beginning of your file directory
 cars = pd.read_csv(r'C:\Users\Documents\cars.csv', index_col=0)
 
+#You can also add another back slash to it as the 2nd method to avoid this error
+#cars = pd.read_csv('C:\\Users\\Documents\\cars.csv', index_col=0)
+
 print(cars)
