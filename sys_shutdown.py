effe6460c9d8e9eda839512d1725d0c12734d2b5
wiskky <adeniyi.wisdom@gmail.com>
2022-05-10 20:41:24 +0100

    commit file

--- sys_shutdown.py
+++ sys_shutdown.py
@@ -1,7 +1,7 @@
 
 #This program shutdown the system/computer in one second
 
-#PLease dont run this program, it will shutdown your system
+#Please dont run this program, it will shutdown your system
 
 import os
 #os.system("shutdown /s /t 5")
